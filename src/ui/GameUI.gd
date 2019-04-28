91788540d8149f04c0048eb855f1c57161b961ee
Leandro Motta Barros <lmb@stackedboxes.org>
2019-04-28 17:49:33 -0300

    Remove useless info from the GUI

--- src/ui/GameUI.gd
+++ src/ui/GameUI.gd
@@ -3,14 +3,7 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	$Panel/Score/Value.text = str(Globals.gameState.score)
-	$Panel/RawMetal/Count.text = str(Globals.gameState.stocks[Globals.MintageState.RAW_METAL])
-	$Panel/MoltenMetal/Count.text = str(Globals.gameState.stocks[Globals.MintageState.MOLTEN_METAL])
-	$Panel/WetPlanchets/Count.text = str(Globals.gameState.stocks[Globals.MintageState.WET_PLANCHET])
-	$Panel/Planchets/Count.text = str(Globals.gameState.stocks[Globals.MintageState.PLANCHET])
-	$Panel/Coins/Count.text = str(Globals.gameState.stocks[Globals.MintageState.COIN])
-
 	_updateCoinRateModifiers(delta)
-	
 	$HelpText.text = Globals.helpText
 
 
